eb1b15aba396994c03293982d92bb89de9b08b38
Uedder <ueddergomes100@gmail.com>
2026-07-01 22:58:10 -0300

    Bind server to all interfaces

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { createServer } = require("node:http");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = process.env.HOSTNAME || "0.0.0.0";
+const hostname = "0.0.0.0";
 const port = Number(process.env.PORT || 3000);
 
 const app = next({ dev, hostname, port });
